f05bcad75a35a1b7f172b8104e43602f4b38aa98
RD320.FIT.UET <66037131+oh-noo-Q@users.noreply.github.com>
2021-10-31 20:55:00 +0700

    update box

--- src/main/java/BlackBox.java
+++ src/main/java/BlackBox.java
@@ -1,34 +1,41 @@
 public class BlackBox {
     public static final int priceEach = 150;
-
     public static String checkout(int quantity, int cumulativePoints) {
         double discount = 0;
         String total = "";
         if(cumulativePoints < 0 || cumulativePoints > 100 || quantity < 1 || quantity > 200) {
             total = "Invalid input";
-            return total;
         }
-        if(1 <= quantity && 10 > quantity) {
-            if(cumulativePoints == 0){}
-            else if(cumulativePoints > 0 && cumulativePoints < 40) discount = 0.02;
-            else discount = 0.04;
+        else {
+            if (1 <= quantity && 10 > quantity) {
+                if (cumulativePoints == 0) {}
+                else if (cumulativePoints > 0 && cumulativePoints < 40)
+                    discount = 0.02;
+                else discount = 0.04;
+            }
+            else if (10 <= quantity && 60 > quantity) {
+                if (cumulativePoints == 0)
+                    discount = 0.02;
+                else if (cumulativePoints > 0 && cumulativePoints < 40)
+                    discount = 0.04;
+                else discount = 0.06;
+            }
+            else if (60 <= quantity && 120 > quantity) {
+                if (cumulativePoints == 0)
+                    discount = 0.06;
+                else if (cumulativePoints > 0 && cumulativePoints < 40)
+                    discount = 0.08;
+                else discount = 0.1;
+            }
+            else if (120 <= quantity && 200 >= quantity) {
+                if (cumulativePoints == 0)
+                    discount = 0.1;
+                else if (cumulativePoints > 0 && cumulativePoints < 40)
+                    discount = 0.12;
+                else discount = 0.15;
+            }
+            total = Double.toString(priceEach * (double) quantity * (1 - discount));
         }
-        if(10 <= quantity && 60 > quantity) {
-            if(cumulativePoints == 0) discount = 0.02;
-            else if(cumulativePoints > 0 && cumulativePoints < 40) discount = 0.04;
-            else discount = 0.06;
-        }
-        if(60 <= quantity && 120 > quantity) {
-            if(cumulativePoints == 0) discount = 0.06;
-            else if(cumulativePoints > 0 && cumulativePoints < 40) discount = 0.08;
-            else discount = 0.1;
-        }
-        if(120 <= quantity && 200 >= quantity) {
-            if(cumulativePoints == 0) discount = 0.1;
-            else if(cumulativePoints > 0 && cumulativePoints < 40) discount = 0.12;
-            else discount = 0.15;
-        }
-        total = Double.toString(priceEach * (double)quantity * (1 - discount));
         return total;
     }
 }
